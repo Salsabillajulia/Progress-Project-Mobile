89646eaeb5881d0eb52bd39ba8aaf74116f077af
Salsabillajulia <salsahira123@gmail.com>
2021-12-14 15:59:26 +0700

    progress mobile

---
+++ app/src/main/java/com/zsr/foodies/activities/TentangActivity.java
@@ -0,0 +1,15 @@
+package com.zsr.foodies.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zsr.foodies.R;
+
+public class TentangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+    }
+}
